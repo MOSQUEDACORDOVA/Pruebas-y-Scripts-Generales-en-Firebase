5d4ac28dd79579749a411e30a08f142572793ee8
Isaac Mosqueda <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2025-04-22 22:32:49 -0500

    firebase

--- functions/index.js
+++ functions/index.js
@@ -120,12 +120,185 @@ exports.obtenerProductosTiendaNube = functions.https.onRequest((req, res) => {
   });
 });
 
+/**
+ * Procesa un nuevo documento en la colección "orders".
+ *
+ * @param {functions.firestore.DocumentSnapshot} event
+ * Evento que contiene los datos del documento creado.
+ * @param {string} orderId ID de la orden.
+ * @param {string} storeId ID de la tienda.
+ */
+async function procesarNuevoDocumentoEnOrders(event, orderId, storeId) {
+  await db.collection("logs_firebase_functions")
+      .doc().set({
+        message: "Si entra en procesarNuevoDocumentoEnOrders",
+        storeId: storeId,
+      });
+
+  // Buscar la tienda asociada al store_id
+  const tiendaDoc = await db
+      .collection("tiendas")
+      .where("user_id", "==", storeId.toString())
+      .limit(1)
+      .get();
+
+  if (tiendaDoc.empty) {
+    await db.collection("logs_firebase_functions")
+        .doc().set({message: "No se encontró la tienda asociada al store_id"});
+    return;
+  }
+
+  const tiendaData = tiendaDoc.docs[0].data();
+  const tiendaToken = tiendaData.token;
+
+  const options = {
+    hostname: "api.tiendanube.com",
+    path: `/v1/${storeId}/orders/${orderId}`,
+    method: "GET",
+    headers: {
+      "Authentication": `bearer ${tiendaToken}`,
+      "User-Agent": "SistemaNube/1.0",
+    },
+  };
+
+  const request = https.request(options, async (response) => {
+    await db.collection("logs_firebase_functions")
+        .doc().set({message: "Si ejecuta api.tiendanube.com"});
+
+    let responseData = "";
+
+    response.on("data", (chunk) => {
+      responseData += chunk;
+    });
+
+    response.on("end", async () => {
+      if (response.statusCode === 200) {
+        const orderDetails = JSON.parse(responseData);
+
+        await db.collection("logs_firebase_functions")
+            .doc().set({message: "tienda responde 200"});
+
+        const clienteRef = await (async () => {
+          const clienteQuery = await db.collection("clientes")
+              .where("LsCustomer", "==", orderDetails.customer.id)
+              .limit(1)
+              .get();
+          return clienteQuery.empty ? null : clienteQuery.docs[0].ref;
+        })();
+
+        await db.collection("historial_puntos").add({
+          puntos: orderDetails.total * tiendaData.equivalencia_puntos,
+          tienda: tiendaDoc.docs[0].ref,
+          cliente: clienteRef,
+          tipo: true,
+          order_id: orderDetails.id,
+          orden: event.data.ref,
+          equivalencia: tiendaData.equivalencia_puntos,
+          motivoExterno: "Hiciste una compra",
+          motivoInterno: "Hizo una compra",
+        });
+
+        // Agregar puntos al cliente
+        if (clienteRef) {
+          await clienteRef.update({
+            puntos: admin.firestore.FieldValue.increment(
+                orderDetails.total * tiendaData.equivalencia_puntos,
+            ),
+          });
+        }
+
+        // Actualización de la orden
+        await event.data.ref.update({
+          total: orderDetails.total,
+          customer_id: orderDetails.customer.id,
+          tienda: tiendaDoc.docs[0].ref,
+          cliente: clienteRef,
+        });
+
+        if (
+          Array.isArray(orderDetails.coupon) &&
+          orderDetails.coupon.length > 0
+        ) {
+          await db.collection("logs_firebase_functions")
+              .doc().set({message: "Si detecta el cupon"});
+
+          const couponId = orderDetails.coupon[0].id;
+
+          if (couponId) {
+            await db.collection("logs_firebase_functions")
+                .doc().set({
+                  message: "si entra en couponId",
+                  IDCuponTiendaNube: couponId});
+
+            const cuponQuery = await db.collection("cupones")
+                .where("IDCuponTiendaNube", "==", couponId.toString())
+                .limit(1)
+                .get();
+            if (!cuponQuery.empty) {
+              await db.collection("logs_firebase_functions")
+                  .doc().set({message:
+                    "Si tiene IDCuponTiendaNube"});
+
+              const puntosDescontar =
+                orderDetails.coupon[0].value * tiendaData.equivalencia_puntos;
+
+              await db.collection("logs_firebase_functions")
+                  .doc().set({
+                    message: "Si tiene IDCuponTiendaNube",
+                    IDCuponTiendaNube: couponId,
+                    puntosDescontar: puntosDescontar});
+              // Descontar puntos al cliente
+              if (clienteRef) {
+                await clienteRef.update({
+                  puntos: admin.firestore.FieldValue.increment(
+                      -puntosDescontar),
+                });
+
+                // Registrar el descuento en el historial de puntos
+                await db.collection("historial_puntos").add({
+                  puntos: puntosDescontar,
+                  tienda: tiendaDoc.docs[0].ref,
+                  cliente: clienteRef,
+                  tipo: false, // Indica que es un descuento
+                  order_id: orderDetails.id,
+                  orden: event.data.ref,
+                  equivalencia: tiendaData.equivalencia_puntos,
+                  motivoExterno: "Aplicaste un cupón",
+                  motivoInterno: `Aplicó el cupón: ${couponId}`,
+                });
+              }
+            }
+          }
+        } else {
+          console.warn("No se detectó un cupón válido en la orden.");
+        }
+      } else {
+        console.error("Error al obtener el total de la orden", {
+          token: tiendaToken,
+          respuesta: responseData,
+        });
+      }
+    });
+  });
+
+  request.on("error", async (error) => {
+    await db.collection("logs_firebase_functions").doc().set({
+      error: error.message,
+      stack: error.stack,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    console.error("Error al procesar la orden", error.message);
+  });
+
+  request.end();
+}
+
 exports.webhookOrderPaid = functions.https.onRequest((req, res) => {
   cors(corsOptions)(req, res, async () => {
     const orderData = req.body;
 
     try {
-    // TEST await db.collection("logs_firebase_functions").doc().set(orderData);
+      // Verificar si la orden ya existe
       const ordersQuery = await db
           .collection("orders")
           .where("id", "==", orderData.id)
@@ -137,16 +310,28 @@ exports.webhookOrderPaid = functions.https.onRequest((req, res) => {
         });
       }
 
-      await db.collection("orders").add({
+      // Agregar la nueva orden y obtener la referencia del documento
+      const docRef = await db.collection("orders").add({
         ...orderData,
         createdAt: admin.firestore.FieldValue.serverTimestamp(),
       });
+
+      // Obtener el documento recién creado
+      const newOrderDoc = await docRef.get();
+
+      // Simular el evento para procesar el nuevo documento
+      const simulatedEvent = {
+        data: newOrderDoc,
+      };
+      await procesarNuevoDocumentoEnOrders(simulatedEvent,
+          orderData.id, orderData.store_id);
 
       // Respuesta exitosa
       res.status(200).json({
         messageCF: "Orden procesada y registrada exitosamente",
       });
     } catch (error) {
+      // Manejo de errores
       await db.collection("logs_firebase_functions").doc().set({
         error: error.message,
         stack: error.stack,
@@ -159,143 +344,3 @@ exports.webhookOrderPaid = functions.https.onRequest((req, res) => {
     }
   });
 });
-
-exports.detectarNuevoDocumentoEnOrders = functions.firestore.onDocumentCreated(
-    "orders/{orderId}",
-    async (event) => {
-      try {
-        const orderData = event.data.data();
-
-        // Buscar la tienda asociada al store_id
-        const tiendaDoc = await db
-            .collection("tiendas")
-            .where("user_id", "==", orderData.store_id)
-            .limit(1)
-            .get();
-
-        if (tiendaDoc.empty) {
-          console.error("No se encontró la tienda asociada al store_id");
-          return;
-        }
-
-        const tiendaData = tiendaDoc.docs[0].data();
-        const tiendaToken = tiendaData.token;
-
-        const options = {
-          hostname: "api.tiendanube.com",
-          path: `/v1/${orderData.store_id}/orders/${orderData.id}`,
-          method: "GET",
-          headers: {
-            "Authentication": `bearer ${tiendaToken}`,
-            "User-Agent": "SistemaNube/1.0",
-          },
-        };
-
-        const request = https.request(options, (response) => {
-          let responseData = "";
-
-          response.on("data", (chunk) => {
-            responseData += chunk;
-          });
-
-          response.on("end", async () => {
-            if (response.statusCode === 200) {
-              const orderDetails = JSON.parse(responseData);
-
-              const clienteRef = await (async () => {
-                const clienteQuery = await db.collection("clientes")
-                    .where("LsCustomer", "==", orderDetails.customer.id)
-                    .limit(1)
-                    .get();
-                return clienteQuery.empty ? null : clienteQuery.docs[0].ref;
-              })();
-
-              await db.collection("historial_puntos").add({
-                puntos: orderDetails.total * tiendaData.equivalencia_puntos,
-                tienda: tiendaDoc.docs[0].ref,
-                cliente: clienteRef,
-                tipo: true,
-                order_id: orderDetails.id,
-                orden: event.data.ref,
-                equivalencia: tiendaData.equivalencia_puntos,
-                motivoExterno: "Hiciste una compra",
-                motivoInterno: "Hizo una compra",
-              });
-
-              // Agregar puntos al cliente
-              if (clienteRef) {
-                await clienteRef.update({
-                  puntos: admin.firestore.FieldValue.increment(
-                      orderDetails.total * tiendaData.equivalencia_puntos,
-                  ),
-                });
-              }
-
-              // Actualización de la orden
-              await event.data.ref.update({
-                total: orderDetails.total,
-                customer_id: orderDetails.customer.id,
-                tienda: tiendaDoc.docs[0].ref,
-                cliente: clienteRef,
-              });
-
-              // Verificar si existe un cupón en la orden
-              if (orderDetails.coupon && orderDetails.coupon.id) {
-                const cuponQuery = await db.collection("cupones")
-                    .where("IDCuponTiendaNube", "==", orderDetails.coupon.id)
-                    .limit(1)
-                    .get();
-                if (!cuponQuery.empty) {
-                  const puntosDescontar =
-                    orderDetails.coupon.value *
-                    tiendaData.equivalencia_puntos;
-
-                  // Descontar puntos al cliente
-                  if (clienteRef) {
-                    await clienteRef.update({
-                      puntos: admin.firestore.FieldValue.increment(
-                          -puntosDescontar),
-                    });
-
-                    // Registrar el descuento en el historial de puntos
-                    await db.collection("historial_puntos").add({
-                      puntos: -puntosDescontar,
-                      tienda: tiendaDoc.docs[0].ref,
-                      cliente: clienteRef,
-                      tipo: false, // Indica que es un descuento
-                      order_id: orderDetails.id,
-                      orden: event.data.ref,
-                      equivalencia: tiendaData.equivalencia_puntos,
-                      motivoExterno: "Aplicaste un cupón",
-                      motivoInterno: `Aplicó el cupon: ` +
-                        `${orderDetails.coupon.id}`,
-                    });
-                  }
-                }
-              }
-            } else {
-              console.error("Error al obtener el total de la orden", {
-                token: tiendaToken,
-                respuesta: responseData,
-              });
-            }
-          });
-        });
-
-        request.on("error", async (error) => {
-          await db.collection("logs_firebase_functions").doc().set({
-            error: error.message,
-            stack: error.stack,
-            timestamp: admin.firestore.FieldValue.serverTimestamp(),
-          });
-          console.error("Error al procesar la orden", error.message);
-        });
-
-        request.end();
-      } catch (error) {
-        console.error(
-            `Error en detectarNuevoDocumentoEnOrders: ${error.message}`,
-        );
-      }
-    },
-);
